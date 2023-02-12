18cbac232bc34d013f2a222e8b0d72bcd8080c29
Vishal Bhaskar <bhaskarvishal120@gmail.com>
2023-02-12 07:57:14 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const getColor = () => {
+    // Hex Code
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    const h2=document.getElementById("color-code")
+    //h2.innerText = randomCode;
+    h2.innerHTML = "<i>" + randomCode+"</i>"
+
+    //navigator.clipboard.writeText(randomCode)
+}
+
+//event call
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+// init call
+getColor();
